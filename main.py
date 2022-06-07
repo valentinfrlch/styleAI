e17de4d88def74f9949b408aa5e87213232a371a
valentinfrlch <valentin.froehlich@gmail.com>
2022-06-07 20:31:55 +0200

    still useless

--- main.py
+++ main.py
@@ -15,15 +15,24 @@ def get_images(search_term, number_of_images):
     html = requests.get(url).text
     # parse the html
     soup = BeautifulSoup(html, 'html.parser')
-    # get the images
-    # only get images with a height of at least 200
-    # if img.get('height') >= 200
-    images = [img for img in soup.find_all('img')]
-    # save the images
-    for img in images[:number_of_images]:
-        if img.startswith('http'):
-            with open("dataset/" + search_term + images.index(img) + '.jpg', 'wb') as f:
-                f.write(requests.get(img).content)
+    # get all links
+    links = soup.find_all('a')
+    
+    # get the images for the first 10 links
+    for link in links[:number_of_images]:
+        try:
+            # get the image url
+            img_url = link.get('href')[7:].split('&sa=')[0]
+            print(link)
+            # get the image name
+            img_name = img_url.split('/')[-1]
+            # get the image
+            img_data = requests.get(img_url).content
+            # save the image
+            with open(os.path.join('images', img_name), 'wb') as handler:
+                handler.write(img_data)
+        except:
+            error('Error: image not found')
 
 
 get_images("Biohackers", 10)
